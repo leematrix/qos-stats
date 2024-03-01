5e148e7b3bb2cc5c3da544b6df08d21b4d992fb0
李智骁 <hobby.li@tuya.com>
2024-03-01 19:32:56 +0800

    add trace of nse

--- conf/config.go
+++ conf/config.go
@@ -3,6 +3,6 @@ package conf
 var OpenLog = true
 var StatsWindowsTimeMs int64 = 1 * 60000
 var StatsWindowsCount = 1000
-var TraceDomainName = "stream-cn.wgine.com"
+var TraceDomainName = ""
 var TracePort = 443
-var TraceAuth = "WbE0DajU9xb0k1uUEaUn"
+var TraceAuth = ""
