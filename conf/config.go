9af2aaab4cf942e39050f6e2faccdd5f5ada2bc2
李智骁 <hobby.li@tuya.com>
2024-03-05 14:46:55 +0800

    add trace enable

--- conf/config.go
+++ conf/config.go
@@ -3,6 +3,7 @@ package conf
 var OpenLog = true
 var StatsWindowsTimeMs int64 = 1 * 60000
 var StatsWindowsCount = 1000
+var TraceEnable = false
 var TraceDomainName = ""
 var TracePort = 443
 var TraceAuth = ""
