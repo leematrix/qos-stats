7485adc943dfef3bb654203ae1ec964e95cbf47b
李智骁 <hobby.li@tuya.com>
2024-03-05 14:47:06 +0800

    add trace enable

--- biz/trace.go
+++ biz/trace.go
@@ -1,48 +1,48 @@
 package biz
 
 import (
-    "encoding/json"
-    "fmt"
-    "github.com/gorilla/websocket"
-    "qos-stats/conf"
-    "qos-stats/data"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"qos-stats/conf"
+	"qos-stats/data"
 )
 
 var wsCli *websocket.Conn
 var nseStatsMsgChan = make(chan NseStats, 1024)
 
 func TraceStart() {
-    if !conf.TraceEnable {
-        return
-    }
+	if !conf.TraceEnable {
+		return
+	}
 
-    ws, err := data.NewWsClient()
-    if err != nil {
-        fmt.Println("New ws client failed, err: ", err)
-    } else {
-        wsCli = ws
-    }
+	ws, err := data.NewWsClient()
+	if err != nil {
+		fmt.Println("New ws client failed, err: ", err)
+	} else {
+		wsCli = ws
+	}
 
-    for {
-        select {
-        case msg := <-nseStatsMsgChan:
-            marshal, _ := json.Marshal(msg)
-            if err = wsCli.WriteMessage(websocket.BinaryMessage, marshal); err != nil {
-                fmt.Println("send ws msg failed, err:", err)
-                wsCli, _ = data.NewWsClient()
-            }
-            fmt.Println("send ws msg ok.")
-        }
-    }
+	for {
+		select {
+		case msg := <-nseStatsMsgChan:
+			marshal, _ := json.Marshal(msg)
+			if err = wsCli.WriteMessage(websocket.BinaryMessage, marshal); err != nil {
+				fmt.Println("send ws msg failed, err:", err)
+				wsCli, _ = data.NewWsClient()
+			}
+			fmt.Println("send ws msg ok.")
+		}
+	}
 }
 
 func TraceIncoming(msg NseStats) {
-    if !conf.TraceEnable {
-        return
-    }
+	if !conf.TraceEnable {
+		return
+	}
 
-    select {
-    case nseStatsMsgChan <- msg:
-    default:
-    }
+	select {
+	case nseStatsMsgChan <- msg:
+	default:
+	}
 }
